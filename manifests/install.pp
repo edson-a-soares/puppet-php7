897dfa295eace43109de01f13250194201921200
Edson A. Soares <maxmontenegro@ig.com.br>
2016-09-12 16:17:57 -0300

    It has been changed the use of Exec to Package to install PHP7 main package.

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,9 +4,8 @@ class php7::install {
 
   apt::ppa { 'ppa:ondrej/php': } ->
 
-  exec { "php7-installation":
-    command	=> "sudo apt-get install --yes php7.0",
-    path	=> [ "/usr/bin", "/bin" ],
+  package { "php7.0":
+    ensure => installed
   }
 
 }
