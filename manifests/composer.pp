7870177d54e548eac0ab1c239db6bd0fc7b221a7
Edson A. Soares <maxmontenegro@ig.com.br>
2016-11-16 12:17:39 -0300

    Refactoring on the composer installing.

--- manifests/composer.pp
+++ manifests/composer.pp
@@ -1,40 +1,38 @@
-define php7::composer ($directory = $title, $home_user, $filename = 'composer.phar', $group = 'root') {
+define php7::composer ($directory = $title, $home_user, $filename = 'composer.phar', $group = 'root', $hash_code) {
 
   Class[ "php7::install" ] -> PHP7::Composer[$title]
 
+  $composer_file_name = "composer-setup.php"
+
   exec { "composer-downloading":
-      command => "php -r \"copy('https://getcomposer.org/installer', 'composer-setup.php');\"",
-      cwd     => "/home/$home_user",
-      onlyif  => "test ! -f $directory/$filename",
-      path    => [ "/bin", "/usr/local/bin", "/usr/bin" ],
-      notify  => Exec[ "composer-checking" ],
-  } ->
+    command => "wget https://getcomposer.org/installer --directory-prefix=/home/$home_user -O $composer_file_name",
+    onlyif  => "test ! -f $directory/$filename",
+    path    => [ "/bin", "/usr/local/bin", "/usr/bin" ],
+    notify  => Exec[ "composer-checking" ],
+  }
 
   exec { "composer-checking":
-      command     => "php -r \"if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;\"",
-      cwd         => "/home/$home_user",
-      path        => [ "/bin", "/usr/local/bin", "/usr/bin" ],
-      subscribe   => Exec[ "composer-downloading" ],
-      refreshonly => true,
-  } ->
+    command     => "php -r \"if (hash_file('SHA384', '/home/$home_user/$composer_file_name') === '$hash_code') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('/home/$home_user/$composer_file_name'); } echo PHP_EOL;\"",
+    path        => [ "/bin", "/usr/local/bin", "/usr/bin" ],
+    subscribe   => Exec[ "composer-downloading" ],
+    refreshonly => true,
+  }
 
   exec { "composer-setup":
-      command     => "php composer-setup.php --install-dir=$directory --filename=$filename",
-      onlyif      => "test ! -f $directory/$filename",
-      cwd         => "/home/$home_user",
-      group       => $group,
-      user        => root,
-      notify      => Exec[ "composer-self-update" ],
-      environment => [ "HOME=/home/$home_user COMPOSER_HOME=/home/$home_user" ],
-      path        => [ "/bin", "/usr/local/bin", "/usr/bin" ],
-  } ->
+    command     => "php /home/$home_user/$composer_file_name --install-dir=$directory --filename=$filename",
+    onlyif      => "test ! -f $directory/$filename",
+    group       => $group,
+    user        => root,
+    notify      => Exec[ "composer-self-update" ],
+    environment => [ "HOME=/home/$home_user COMPOSER_HOME=/home/$home_user" ],
+    path        => [ "/bin", "/usr/local/bin", "/usr/bin" ],
+  }
 
   exec { "composer-unlink":
-      command     => "php -r \"unlink('composer-setup.php');\"",
-      cwd         => "/home/$home_user",
-      subscribe   => Exec[ "composer-downloading" ],
-      refreshonly => true,
-      path        => [ "/bin", "/usr/local/bin", "/usr/bin" ],
+    command     => "php -r \"unlink('/home/$home_user/$composer_file_name');\"",
+    subscribe   => Exec[ "composer-downloading" ],
+    refreshonly => true,
+    path        => [ "/bin", "/usr/local/bin", "/usr/bin" ],
   } ->
 
   exec { "composer-self-update":
@@ -42,7 +40,7 @@ define php7::composer ($directory = $title, $home_user, $filename = 'composer.ph
     environment => [ "HOME=/home/$home_user COMPOSER_HOME=/home/$home_user" ],
     subscribe   => Exec[ "composer-setup" ],
     refreshonly => true,
-    path        => [ "/bin", "/usr/local/bin", "/usr/bin" ],
+    path        => [ "/bin", "$directory", "/usr/bin" ],
   }
 
 }
